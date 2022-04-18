e54eab7d8fab40572261f3b595bebb30e0478b4b
emityakov <iyao@mail.ru>
2022-04-18 12:37:06 +0300

    first commit

---
+++ app/src/main/java/ru/samsung/itacademy/mdev/task3161_task/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.samsung.itacademy.mdev.task3161_task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
